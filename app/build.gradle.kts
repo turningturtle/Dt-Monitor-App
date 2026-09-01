d6d33e71cdac6b7033ca9a2d9f39059a9927e917
Crazyycrash223 <og937503@gmail.com>
2026-09-01 20:07:01 +0530

    Add app module configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.turningturtle.dtmonitor"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.turningturtle.dtmonitor"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+kotlinOptions { jvmTarget = "17" }
